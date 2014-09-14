96dccd3c8ab9885f2eb8c72e77a48c168eb7e533
Stefan Scherer <scherer_stefan@icloud.com>
2014-09-14 18:28:50 +0200

    updated to Vagrant 1.6.5 and vagrant-vcloud 0.4.2

--- test/vagrant_spec.rb
+++ test/vagrant_spec.rb
@@ -2,11 +2,11 @@ require_relative 'spec_helper'
 
 describe 'Vagrant' do
   describe command('vagrant --version') do
-    it { should return_stdout(/1\.6\.3/) }
+    it { should return_stdout(/1\.6\.5/) }
   end
 
   describe command('vagrant plugin list') do
-    it { should return_stdout(/vagrant-vcloud .0\.4\.1./) }
+    it { should return_stdout(/vagrant-vcloud .0\.4\.2./) }
   end
 
   describe command('vagrant box list') do
