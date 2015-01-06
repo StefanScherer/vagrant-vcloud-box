8124078c55c7cada8836b42438e8eaa3a684a099
Stefan Scherer <scherer_stefan@icloud.com>
2015-01-06 09:22:10 +0100

    test german keyboard should be first

--- test/german_spec.rb
+++ test/german_spec.rb
@@ -3,4 +3,8 @@ describe 'Germany' do
     it { should be_file }
     its(:content) { should match /Europe.Berlin/ }
   end
+
+  describe command('gsettings get org.gnome.desktop.input-sources sources') do
+    its(:stdout) { should match /^\[\('xkb', 'de'\)/ }
+  end
 end
