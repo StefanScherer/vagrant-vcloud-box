9168a84f53a1b43b5c7a35568042d56840fddaa7
ben@iphouse.net <bjtucker@users.noreply.github.com>
2015-02-11 08:35:47 +0000

    Upgrade to Vagrant 1.7.2

--- scripts/install-vagrant.sh
+++ scripts/install-vagrant.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp
-if [ ! -f vagrant_1.7.1_x86_64.deb ]; then
-  echo "Downloading Vagrant 1.7.1"
-  wget -q https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.1_x86_64.deb
-  echo "Installing Vagrant 1.7.1"
-  sudo dpkg -i vagrant_1.7.1_x86_64.deb
-  rm vagrant_1.7.1_x86_64.deb
+if [ ! -f vagrant_1.7.2_x86_64.deb ]; then
+  echo "Downloading Vagrant 1.7.2"
+  wget -q https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
+  echo "Installing Vagrant 1.7.2"
+  sudo dpkg -i vagrant_1.7.2_x86_64.deb
+  rm vagrant_1.7.2_x86_64.deb
 fi
